283786ae0c310b444eee123dec62de6530a001db
vsvld <vsvld@AMNYAM>
2023-09-24 15:31:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace calkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Доступные операции: 1)Сложение; 2)Вычитание; 3)Умножение; 4)Деление");
+            int f = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое число");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double m = Convert.ToDouble(Console.ReadLine());
+            double end;
+            switch (f) 
+            {
+                case 1:
+                    end = n+m;
+                    Console.WriteLine(end);
+                    break;
+                case 2: 
+                    end = n-m; 
+                    Console.WriteLine(end); 
+                    break;
+                case 3:
+                    end = n * m;
+                    Console.WriteLine(end);
+                    break;
+                case 4:
+                    if (m == 0) 
+                    {
+                        Console.WriteLine("Осуждаем деление на 0");
+
+                    }
+                    else 
+                    {
+                        end = n / m;
+                        Console.WriteLine(end);
+                    }
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
