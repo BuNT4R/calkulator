ebbe09cad35bb8d822bd64f1945a692b91b70426
Anastasia <girl-physicist@mail.ru>
2023-09-26 16:04:55 +0300

    comments

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace calkulator
         static void Main(string[] args)
         {
             Console.WriteLine("Доступные операции: 1)Сложение; 2)Вычитание; 3)Умножение; 4)Деление");
-            int f = Convert.ToInt32(Console.ReadLine());
+            int f = Convert.ToInt32(Console.ReadLine()); // следует давать значимые (передающие смысл) имена. Например typeOfTransaction. Кроме того, следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
             Console.WriteLine("Введите первое число");
-            double n = Convert.ToDouble(Console.ReadLine());
+            double n = Convert.ToDouble(Console.ReadLine());// следует давать значимые (передающие смысл) имена. Например number1. Кроме того, следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
             Console.WriteLine("Введите второе число");
-            double m = Convert.ToDouble(Console.ReadLine());
+            double m = Convert.ToDouble(Console.ReadLine());// следует давать значимые (передающие смысл) имена. Например number2. Кроме того, следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
             double end;
             switch (f) 
             {
